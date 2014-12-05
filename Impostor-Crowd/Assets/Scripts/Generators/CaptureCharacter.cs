1cc8d1c5863def2bd9a802a4eea23a798074bf50
Stjerndal <stjernd@kth.se>
2014-12-05 22:05:29 +0100

    Cerise texture.

--- Impostor-Crowd/Assets/Scripts/Generators/CaptureCharacter.cs
+++ Impostor-Crowd/Assets/Scripts/Generators/CaptureCharacter.cs
@@ -73,8 +73,8 @@ public class CaptureCharacter : MonoBehaviour
             blankFrame [i] = Color.clear;
             blankNormalFrame [i] = normalColor;
         }
-        textureMaterial = new Material(Shader.Find("Diffuse"));
-//		textureMaterial = new Material(Shader.Find("Transparent/Diffuse"));
+//        textureMaterial = new Material(Shader.Find("Diffuse"));
+		textureMaterial = new Material(Shader.Find("Transparent/Diffuse"));
         normalMaterial = new Material(Shader.Find("Custom/DisplayNormals"));
         textureMaterial.mainTexture = charTexture;
 		
